97b43942fbe166cd7b0818e4f33f358a86070754
Silvio Rhatto <rhatto@riseup.net>
2013-09-15 15:34:58 -0300

    Removing unused debian templates

--- debian/keyringer.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the keyringer package
-#
-0 4	* * *	root	[ -x /usr/bin/keyringer_maintenance ] && /usr/bin/keyringer_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(keyringer):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="keyringer" command="/usr/bin/keyringer"
